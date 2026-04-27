f5c755495a7f4602b2a799ae95141a5c2b01b3ef
DavDem <davidedemarco088@gmail.com>
2026-04-27 18:24:51 +0000

    es

--- Nodo.java
+++ Nodo.java
@@ -1,1 +1,9 @@
 //Implementare qui la classe Nodo
+class Nodo<T>{
+    T dato;
+    Nodo<T> next;
+    public Nodo(){
+        this.dato=dato;
+        this.next=null;
+    }
+}
--- Pila.java
+++ Pila.java
@@ -1,1 +1,67 @@
-//Implementare qui la classe Pila
+import java.util.NoSuchElementException;
+
+class Nodo<T> {
+    T dato;
+    Nodo<T> next;
+
+    public Nodo(T dato) {
+        this.dato = dato;
+        this.next = null;
+    }
+}
+
+class Pila<T> {
+    private Nodo<T> top;
+
+    public Pila() {
+        this.top = null;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void push(T dato) {
+        Nodo<T> nodo = new Nodo<>(dato);
+        nodo.next = top;
+        top = nodo;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Pila vuota");
+        }
+        T dato = top.dato;
+        top = top.next;
+        return dato;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Pila vuota");
+        }
+        return top.dato;
+    }
+
+    public int size() {
+        int count = 0;
+        Nodo<T> current = top;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Nodo<T> current = top;
+        while (current != null) {
+            sb.append(current.dato).append(" --> ");
+            current = current.next;
+        }
+        sb.append("null");
+        return sb.toString();
+    }
+}
