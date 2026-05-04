e5e05791a47d50cba31f8553ac84f76b46dedee2
DavDem <davidedemarco088@gmail.com>
2026-05-04 14:11:53 +0000

    indolfi

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,36 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+class Parentesi{
+    private static boolean check(Char ap,Char ch){
+        return(ap='(' and ch=')'|| ap='[' and ch=']' || ap='{' and ch='}'  );
+    }
+    public static boolean corispondente(String parentesi){
+            Pila <character> pila= new Pila <>();
+            for(int i=0;i<parentesi.length();i++){
+                 char c=parentesi.charAt(i);
+                 if(c=='{' ||c=='[' || c=='('){
+                    pila.push(c);
 
-// Classe principale, con metodo main
-class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
-
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
-
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+                 }
+                 if(pila.isEmpty()){
+                    return false;
+                 }
+                 char k=pila.pop();
+                 if(!check(k, c)){
+                    return false;
+                 }
+            }
+            return pila.isEmpty();
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("inserisci parentesi");
+        Scanner in=new Scanner(System.in);
+        parentesi=in.nextLine();
+        boolean bil=corispondente(parentesi);
+        if(bil){
+            System.out.pritnln("va bene");
+        }else{
+            System.out.println("non va bene")
+        }
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
